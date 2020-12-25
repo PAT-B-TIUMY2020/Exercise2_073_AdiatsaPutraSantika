7a0349f949e1b558858ecc43499d874e8f40e7d3
AdiatsaPutra <santikosetyo77@gmail.com>
2020-12-25 23:25:54 +0700

    Remove Unnecessary Class

--- Exercise2_073_Adiatsa Putra Santika/Form1.cs
+++ Exercise2_073_Adiatsa Putra Santika/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using ServiceRest_073_Adiatsa_Putra_Santika;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,6 +9,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Exercise2_073_Adiatsa_Putra_Santika.Form1;
 
 namespace Exercise2_073_Adiatsa_Putra_Santika
 {
@@ -157,23 +157,7 @@ namespace Exercise2_073_Adiatsa_Putra_Santika
         }
     }
 
-    class ClassData
-    {
-        public void getData()
-        {
-            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
-            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
 
-            //foreach (var mhs in data)
-            //{
-            //    Console.WriteLine("NIM: " + mhs.nim);
-            //    Console.WriteLine("Nama: " + mhs.nama);
-            //    Console.WriteLine("Prodi: " + mhs.prodi);
-            //    Console.WriteLine("Angkatan: " + mhs.angkatan);
-            //}
-            //Console.ReadLine();
-        }
-    
     }
 
 
