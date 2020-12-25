c89a7a6725151f8f0b99cd4c3c000d6dc6ffc147
AdiatsaPutra <santikosetyo77@gmail.com>
2020-12-25 22:30:12 +0700

    Add Add mahasiswa

--- Exercise2_073_Adiatsa Putra Santika/Form1.cs
+++ Exercise2_073_Adiatsa Putra Santika/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ServiceRest_073_Adiatsa_Putra_Santika;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,7 @@ namespace Exercise2_073_Adiatsa_Putra_Santika
 {
     public partial class Form1 : Form
     {
+        string baseUrl = "http://localhost:1907/";
         public Form1()
         {
             InitializeComponent();
@@ -21,5 +25,44 @@ namespace Exercise2_073_Adiatsa_Putra_Santika
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.nama = textBox1.Text;
+            mhs.nim = textBox2.Text;
+            mhs.prodi = textBox3.Text;
+            mhs.angkatan = textBox4.Text;
+
+            var data = JsonConvert.SerializeObject(mhs);
+                var postdata = new WebClient();
+                postdata.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                string response = postdata.UploadString(baseUrl + "Mahasiswa", data);
+                Console.WriteLine(response);
+
+            
+        }
     }
+
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            //foreach (var mhs in data)
+            //{
+            //    Console.WriteLine("NIM: " + mhs.nim);
+            //    Console.WriteLine("Nama: " + mhs.nama);
+            //    Console.WriteLine("Prodi: " + mhs.prodi);
+            //    Console.WriteLine("Angkatan: " + mhs.angkatan);
+            //}
+            //Console.ReadLine();
+        }
+    
+    }
+
+
 }
---
+++ Server_073_Adiatsa Putra Santika/Form1.cs
@@ -0,0 +1,96 @@
+﻿
+using ServiceRest_073_Adiatsa_Putra_Santika;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel.Channels;
+using System.Windows.Forms;
+
+namespace Server_073_Adiatsa_Putra_Santika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Server Nyala";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding bind = new WebHttpBinding();
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                //ALAMAT BASE ADDRESS
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), bind, "");
+
+
+                //ALAMAT ENDPOINT
+                //wsdl
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior(); //Service Runtime Player
+                smb.HttpGetEnabled = true; //untuk mengaktifkan wsdl (dibuka saat development, tidak untuk dibuka)
+                hostObjek.Description.Behaviors.Add(smb);
+                //mex
+                System.ServiceModel.Channels.Binding mexbind = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbind, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                label1.Text = "Server Nyala";
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding bind = new WebHttpBinding();
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), bind, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                hostObjek.Description.Behaviors.Add(smb);
+                System.ServiceModel.Channels.Binding mexbind = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbind, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Abort();
+                label1.Text = "Silakan Nyalakan Server";
+                button1.Enabled = false;
+                button2.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
